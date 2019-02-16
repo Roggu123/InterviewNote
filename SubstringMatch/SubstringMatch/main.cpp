0add35b0c4f2fbe9b4aac86077e506073d995211
Frank <2946994766@qq.com>
2019-02-16 14:08:12 +0800

    Update main.cpp

--- SubstringMatch/SubstringMatch/main.cpp
+++ SubstringMatch/SubstringMatch/main.cpp
@@ -7,9 +7,40 @@
 //
 
 #include <iostream>
-
+#include <stdlib.h>
+#include <string>
+using namespace std;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    char LongString[8];
+    //int LongString;
+    //char LongString;
+    //string LongString;
+    
+    cout << "Input LongString" << endl;
+    //cin >> LongString;            //成功
+    //cin.get(LongString,8);         //成功
+    cin.getline(LongString,4);    //成功
+    cin.getline(<#char_type *__s#>, <#streamsize __n#>)
+    //getline(cin, LongString);     //无法赋值
+    //gets(LongString);             //成功
+    cout << "Output LongString" <<endl;
+    cout << LongString << endl;
     return 0;
+
 }
+
+
+//int main(int argc, const char * argv[]){
+//    char* pLongString;
+//    std::cout << "Assignment pLongString\n";
+//    //pLongString = LongString;
+//    //cin >> pLongString;           //无法赋值
+//    //cin.get(pLongString, 30);     //成功
+//    //cin.getline(pLongString,10);  //成功
+//    //getline(cin, pLongString);    //无法赋值,编译都通不过
+//    //gets(pLongString);            //无法赋值
+//    cout << "Output pLongString\n";
+//    std::cout << pLongString << endl;
+//    return 0;
+//}
+
