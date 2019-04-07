af3bbf1cb584899d1948500c6be29a04b67a5dea
Frank <2946994766@qq.com>
2019-04-07 14:06:51 +0800

    Add README.md

---
+++ MergeList/MergeList/MergeList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MergeList
+//
+//  Created by Ruogu Lu on 2019/4/5.
+//  Copyright © 2019 Ruogu Lu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
